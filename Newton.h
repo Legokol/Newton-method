9ad6962fb862aff4c8c83953ff4c56b4430ea9fa
Legokol <legokol3@gmail.com>
2021-12-13 10:18:53 +0300

    Redundant counters removed.

--- Newton.h
+++ Newton.h
@@ -10,23 +10,19 @@ public:
 
     double solve(double u0, double (*f)(double), double (*df)(double), double epsilon) {
         double u = u0 - f(u0) / df(u0);
-        int i = 0;
         while (fabs(u - u0) > epsilon) {
             u0 = u;
             u = u0 - f(u0) / df(u0);
-            ++i;
         }
         return u;
     }
 
     double solve(double u0, double u1, double (*f)(double), double epsilon) {
         double u = u1 - f(u1) * (u1 - u0) / (f(u1) - f(u0));
-        int i = 0;
         while (fabs(u - u1) > epsilon) {
             u0 = u1;
             u1 = u;
             u = u1 - f(u1) * (u1 - u0) / (f(u1) - f(u0));
-            ++i;
         }
         return u;
     }
